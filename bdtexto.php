ccf13c8537c617747dc97f16e670c408e7deb83a
santi.domi <santudoscu@gmail.com>
2023-06-22 18:53:29 -0300

    entrega final

--- bdtexto.php
+++ bdtexto.php
@@ -29,7 +29,7 @@ if (isset($_SESSION['admin'])) {
 <h4> NOMBRE DEL PRODUCTO: </h4> Teclado Redragon Kumara K552 <img src="https://i.postimg.cc/MH1JDyCx/1058-producto-1019-producto-tecl-1.jpg" alt=""><h4> VALOR FINAL: </h4>$8450 <h4> COMENTARIO: </h4>Color negro
 <h4> NOMBRE DEL PRODUCTO: </h4> Teclado Noganet Ganon XS <img src="https://i.postimg.cc/SNmv3F8c/D-NQ-NP-2-X-901225-MLA44221312364.webp" alt=""><h4> VALOR FINAL: </h4>$4500 <h4> COMENTARIO: </h4>Color negro 
 <h4> NOMBRE DEL PRODUCTO: </h4> HyperX Cloud Stinger Core Wireless <img src="https://i.postimg.cc/PJXQ3Hcx/hyperx-cloud-stinger-core-wirele.jpg" alt=""><h4> VALOR FINAL: </h4>$7600 <h4> COMENTARIO: </h4>Color blanco
-<h4> NOMBRE DEL PRODUCTO: </h4> Auriculares gamer HyperX Cloud Alpha <img src="https://i.postimg.cc/1Xb4gcpQ/1.jpg" alt=""><h4> VALOR FINAL: </h4>$230000 <h4> COMENTARIO: </h4>Color negro
+<h4> NOMBRE DEL PRODUCTO: </h4> Auriculares gamer HyperX Cloud Alpha <img src="https://i.postimg.cc/1Xb4gcpQ/1.jpg" alt=""><h4> VALOR FINAL: </h4>$15000 <h4> COMENTARIO: </h4>Color negro
 
 
 
---
+++ pedido.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 22-06-2023 a las 22:02:34
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `php_intermedio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedido`
+--
+
+CREATE TABLE `pedido` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `valor` varchar(40) NOT NULL,
+  `imagen` varchar(100) NOT NULL,
+  `descripcion` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `pedido`
+--
+
+INSERT INTO `pedido` (`id`, `nombre`, `valor`, `imagen`, `descripcion`) VALUES
+(1, 'Auriculares gamer Hy', '15000', 'Auriculares gamer HyperX Cloud Alpha', 'Auriculares gamer HyperX Cloud Alpha');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
